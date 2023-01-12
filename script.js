f6c914fdf4938e2ccd12f8686226ca548be6247f
Rahul Kumawat <116284606+kumawat30rahul@users.noreply.github.com>
2023-01-12 10:54:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function threeSum(arr, target) {
 		for(let j = i+1 ; j < arr.length - 1; j++){
 			for(let k = j+1; k< arr.length; k++){
 				ans = arr[i]+arr[j]+arr[k];
-				if(ans == closet){
+				if(ans === target){
 					return ans
 				}else if(Math.abs(ans - target) < Math.abs(closet - target)){
 					closet = ans;
